047764a99a237a9814712e05956dbc2eef622310
Zhibek Tadzhibaeva <jibektad@mail.ru>
2023-03-08 16:55:19 +0600

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 9999 && number < 100000)
+    {
+        string str = number.ToString();
+        if(str[0] == str[4] && str[1] == str[3])
+        Console.WriteLine("Является");
+    
+        else
+        Console.WriteLine("Не является");
+        
+    }
+    else        
+    Console.WriteLine("Это не пятизначное число");
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+Console.WriteLine("Введите координаты трех точек");
+int Ax = Convert.ToInt32(Console.ReadLine());
+int Ay = Convert.ToInt32(Console.ReadLine());
+int Az = Convert.ToInt32(Console.ReadLine());
+int Bx = Convert.ToInt32(Console.ReadLine());
+int By = Convert.ToInt32(Console.ReadLine());
+int Bz = Convert.ToInt32(Console.ReadLine());
+double i = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Bx - By, 2) + Math.Pow(Az - Bz, 2));
+Console.WriteLine("Расстояние между тремя точками на плоскости =  " + Math.Round(i, 2));
